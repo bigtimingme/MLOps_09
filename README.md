88bb9379b7f1bd91f42baa4ce344ac63aaec3815
Francisco Jimenez <fjarechaga@gmail.com>
2023-04-13 20:44:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+
+PROYECTO INDIVIDUAL Nº1
+Machine Learning Operations (MLOps)
+
+Alumno: Francisco Jiménez de Aréchaga
+github: bigtimingme
+
+CONSIGNAS:
+
+1. Hacer las siguientes transformaciones a los archivos csv de las plataformas de streaming:
+
+  Generar campo id: Cada id se compondrá de la primera letra del nombre de la plataforma, seguido del show_id ya presente en los datasets (ejemplo para títulos de Amazon = as123)
+
+  Los valores nulos del campo rating deberán reemplazarse por el string “G” (corresponde al maturity rating: “general for all audiences”)
+
+  De haber fechas, deberán tener el formato AAAA-mm-dd
+
+  Los campos de texto deberán estar en minúsculas, sin excepciones
+
+  El campo duration debe convertirse en dos campos: duration_int y duration_type. El primero será un integer y el segundo un string indicando la unidad de medición de duración: min (minutos) o season (temporadas)
+
+El punto 1 de las consignas está realizado en el archivo proyecto_machine_learning.ipynb
+
+
+2. Desarrollar una API con las siguientes consultas:
+
+  Película con mayor duración con filtros AÑO, PLATAFORMA Y TIPO DE DURACIÓN. (función: get_max_duration(year, platform, duration_type))
+
+  Cantidad de películas por plataforma con un puntaje mayor a XX en determinado año (función: get_score_count(platform, scored, year))
+
+  Cantidad de películas por plataforma con filtro de PLATAFORMA. (función: get_count_platform(platform))
+
+  Actor que más se repite según plataforma y año. (función: get_actor(platform, year))
+
+  Cantidad de contenido por que se produció en un determinado país en un año. (función: prod_per_country(type,country,year))
+
+  Cantidad de contenido total según el rating dado por las reseñas de los usuarios. (función: get:contents(rating))
+
+  El punto 2 está llevado a cabo en el submódulo del repositorio github llamado soyhenry-fastapi. Armé un submódulo en Github porque sino iba a duplicar informacíon y ya tenía hecho el repositorio https://github.com/bigtimingme/soyhenry-fastapi
+
+3. Hacer un Deployment de la API del punto 2:
+  El deployment del sistema de recomendación está en https://github.com/bigtimingme/soyhenry-fastapi
+
+4. Análisis exploratorio de los datos: (Exploratory Data Analysis-EDA):
+  No encontré relaciones interesantes, salvo que el promedio de reseñas está entre 3,3 y 3,8.
+
+5. Sistema de recomendación:
+El sistema de recomendación que creé es un sistema basado en la combinación de la columna title(título) y la columna listed_in(géneros). Usé luego CountVectorizer y cosine_similarity de la librería scikit-learn para crear una matrix de similitud.
+Al ser imperativo la inclusión del sistema de recomendación en el script de la API, creé una columna en el DataFrame de las consultas que se llama 'recommender' y puse los 5 títulos más recomendados en orden descendente para cada item.
+
+6. Video: 
+  Hacer un video mostrando el resultado de las consultas propuestas y del modelo de ML entrenado.
+  link al video:
+
---
+++ soyhenry-fastapi/main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+app.setup()
+
+total_df = pd.read_csv('df_fastapi.csv')
+
+@app.get('/')
+async def root():
+    return {'Proyecto Individual de SOY HENRY del alumno Francisco Jimenez de Arechaga'}
+
+@app.get('/get_max_duration/{year}/{plat}/{dur_type}')
+async def get_max_duration(year:int=None,plat:str=None,dur_type:str='min'):
+
+    mytitle = total_df[(total_df['release_year']==year) & (total_df['platform']==plat) & (total_df['type']=='movie') &
+                       (total_df['duration_type']==dur_type)]
+    maxi = mytitle[mytitle['duration_int'] == max(mytitle['duration_int'])][['title','duration_int']]
+    title = maxi.iloc[0].values[0]
+    duration = maxi.iloc[0].values[1]
+        
+    return {'title':title,'duration':duration,'duration_type':dur_type}
+
+@app.get('/get_score_count/{platform}/{scored}/{year}')
+async def get_score_count(platform:str,scored:float,year:int):
+    a = int(total_df[(total_df['platform']==platform)&(total_df['scored']>scored)&(total_df['release_year']==year)]['title'].count())
+    return {f'cantidad de contents arriba de {scored} puntos': a}
+
+@app.get('/get_count_platform/{platform}')
+def get_count_platform(platform:str):
+    a = int(total_df[(total_df['platform']==platform) & (total_df['type']=='movie')]['title'].count())
+    return {f'cantidad de contents en {platform}':a}
+
+@app.get('/get_actor/{platform}/{year}')
+def get_actor(platform:str,year:int):
+    if platform=='hulu':
+        return {'data no disponible para Hulu'}
+    else:
+        a = total_df[(total_df['platform']==platform)&(total_df['release_year']==year)]['cast']
+        actorslist = []
+        for i in a:
+            if type(i) == str:
+                b = i.split(',')
+                for i in b:
+                    if i.startswith(' '):
+                        actorslist.append(i[1:])
+                    else:
+                        actorslist.append(i)
+            else:
+                pass
+
+    a = actorslist
+    counter = 0
+    num = a[0]
+    for i in a:
+        curr_frequency = a.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+    return {'actor':num,'cantidad de veces':a.count(num)}
+
+
+@app.get('/prod_per_country')
+async def products_per_country(country:str,year:int,content:str):
+    a = len(total_df[(total_df['country']== country) & (total_df['release_year']== year) & (total_df['type']==content)])
+    return {f'cantidad de películas de {country} en el year {year} del tipo {content} es: {a}'}
+
+@app.get('/get_contents')
+async def get_contents(rating):
+    contenido = len(total_df[total_df['scored']==rating])
+    return {f'Cantidad de contenido con puntaje {rating} es: {contenido}'}
+
+@app.get('/recommender')
+async def recommender(movie):
+    a = total_df[total_df['title']==movie]['recommender']
+    recommender = [i for i in a]
+    return {f'contents recomendadas para {movie} son: {recommender}'}
